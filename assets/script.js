9d5754879dd0c38c9cb0f0df4a517c37f25807e3
jbramble18 <jbramble.rls@gmail.com>
2019-02-01 19:13:10 -0600

    Professional-Portfolio

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,7 @@ $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
   });
+
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
